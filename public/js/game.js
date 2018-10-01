e08b0d5531e11aa530f04d50ef674ff0ecc87ec6
hazkaz <harikrishnan527@gmail.com>
2018-10-02 02:23:29 +0530

    Setup player tracking and updating across machines
    1. added gitignore
    2. removed node_modules
    3. add package-lock
    4. added eslint

--- public/js/game.js
+++ public/js/game.js
@@ -1,71 +1,118 @@
-var playerSize = 30;
-var config = {
-    type: Phaser.AUTO,
-    parent: 'phaser-example',
-    width: 800,
-    height: 600,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            debug: false,
-            gravity: { y: 0 }
-        }
+const playerSize = 30;
+const playerSpeed = 150;
+const config = {
+  type: Phaser.AUTO,
+  parent: 'phaser-example',
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false,
+      gravity: { y: 0 },
     },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
+  },
+  scene: {
+    preload,
+    create,
+    update,
+  },
 };
 
-var game = new Phaser.Game(config);
+const game = new Phaser.Game(config);
 
 function preload() {
-    this.load.image('bluePlayer', 'assets/characterBlue (3).png');
-    this.load.image('redPlayer', 'assets/characterRed (3).png');
+  this.load.image('bluePlayer', 'assets/characterBlue (3).png');
+  this.load.image('redPlayer', 'assets/characterRed (3).png');
+  this.load.spritesheet('ground', 'assets/groundGrass_mown.png',{frameWidth:55,frameHeight:55});
 }
 
 function create() {
-    var self = this;
-    this.socket = io();
-    this.socket.on('currentPlayers', function (players) {
-        Object.keys(players).forEach(function (id) {
-            if (players[id].playerId === self.socket.id) {
-                addPlayer(self, players[id]);
-            }
-        });
+  back = this.add.tileSprite(400,300, 800,600, 'mushroom').setAlpha(0.2);
+  this.socket = io();
+  this.otherPlayers = this.physics.add.group();
+  this.socket.on('currentPlayers', (players) => {
+    Object.keys(players).forEach((id) => {
+      if (players[id].playerId === this.socket.id) {
+        addPlayer(this, players[id]);
+      } else {
+        addOtherPlayers(this, players[id]);
+      }
     });
-    this.socket.on('newPlayer', function (playerInfo) {
-        addOtherPlayers(self, playerInfo);
+  });
+  this.socket.on('newPlayer', (playerInfo) => {
+    addOtherPlayers(this, playerInfo);
+  });
+  this.socket.on('playerMoved', (playerInfo) => {
+    this.otherPlayers.getChildren().forEach((otherPlayer) => {
+      if (otherPlayer.playerId === playerInfo.playerId) {
+        otherPlayer.setRotation(playerInfo.rotation);
+        otherPlayer.setPosition(playerInfo.x, playerInfo.y);
+      }
+      this.update();
     });
-    this.socket.on('disconnect', function (playerId) {
-        self.otherPlayers.getChildren().forEach(function (otherPlayer) {
-            if (playerId === otherPlayer.playerId) {
-                otherPlayer.destroy();
-            }
-        });
+  });
+  this.socket.on('disconnect', (playerId) => {
+    this.otherPlayers.getChildren().forEach((otherPlayer) => {
+      if (playerId === otherPlayer.playerId) {
+        otherPlayer.destroy();
+      }
     });
+  });
+  this.cursors = this.input.keyboard.createCursorKeys();
 }
 
-function update() { }
+function update() {
+  if (this.player) {
+    this.physics.world.collide(this.player, this.otherPlayers);
+    if (this.cursors.left.isDown) {
+      this.player.setAngularVelocity(-150);
+    } else if (this.cursors.right.isDown) {
+      this.player.setAngularVelocity(150);
+    }
+
+
+    if (this.cursors.up.isDown) {
+      this.physics.velocityFromRotation(this.player.rotation, 100, this.player.body.velocity);
+    }
+    if (this.cursors.down.isDown) {
+      this.physics.velocityFromRotation(this.player.rotation, -50, this.player.body.velocity);
+    }
+
+    this.physics.world.wrap(this.player, 5);
+    const x = this.player.x;
+    const y = this.player.y;
+    const r = this.player.rotation;
+    if (this.player.oldPosition && (x !== this.player.oldPosition.x || y !== this.player.oldPosition.y || r !== this.player.oldPosition.rotation)) {
+      this.socket.emit('playerMovement', { x: this.player.x, y: this.player.y, rotation: this.player.rotation });
+    }
+
+    // save old position data
+    this.player.oldPosition = {
+      x: this.player.x,
+      y: this.player.y,
+      rotation: this.player.rotation,
+    };
+  }
+}
 
 function addPlayer(self, playerInfo) {
-    if (playerInfo.team === 'red') {
-        self.ship = self.physics.add.image(playerInfo.x, playerInfo.y, 'redPlayer').setOrigin(0.5, 0.5).setDisplaySize(playerSize, playerSize);
-    } else {
-        self.ship = self.physics.add.image(playerInfo.x, playerInfo.y, 'bluePlayer').setOrigin(0.5, 0.5).setDisplaySize(playerSize, playerSize);
-    }
-    self.ship.setDrag(100);
-    self.ship.setAngularDrag(100);
-    self.ship.setMaxVelocity(200);
+  if (playerInfo.team === 'red') {
+    self.player = self.physics.add.image(playerInfo.x, playerInfo.y, 'redPlayer').setOrigin(0.5, 0.5).setDisplaySize(playerSize, playerSize);
+  } else {
+    self.player = self.physics.add.image(playerInfo.x, playerInfo.y, 'bluePlayer').setOrigin(0.5, 0.5).setDisplaySize(playerSize, playerSize);
+  }
+  self.player.setDrag(100);
+  self.player.setAngularDrag(1000);
+  self.player.setMaxVelocity(200);
 }
 function addOtherPlayers(self, playerInfo) {
-    let otherPlayer;
-    if (playerInfo.team === 'red') {
-        otherPlayer = self.add.sprite(playerInfo.x, playerInfo.y, 'redPlayer').setOrigin(0.5, 0.5).setDisplaySize(playerSize, playerSize);
-    } else {
-        otherPlayer = self.add.sprite(playerInfo.x, playerInfo.y, 'bluePlayer').setOrigin(0.5, 0.5).setDisplaySize(playerSize, playerSize);
-    }
-    otherPlayer.playerId = playerInfo.playerId;
-    self.otherPlayers.add(otherPlayer);
+  let otherPlayer;
+  if (playerInfo.team === 'red') {
+    otherPlayer = self.add.sprite(playerInfo.x, playerInfo.y, 'redPlayer').setOrigin(0.5, 0.5).setDisplaySize(playerSize, playerSize);
+  } else {
+    otherPlayer = self.add.sprite(playerInfo.x, playerInfo.y, 'bluePlayer').setOrigin(0.5, 0.5).setDisplaySize(playerSize, playerSize);
+  }
+  otherPlayer.playerId = playerInfo.playerId;
+  self.otherPlayers.add(otherPlayer);
 }
--- server.js
+++ server.js
@@ -1,37 +1,43 @@
-var express = require('express');
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-var players = {};
+const express = require('express');
 
-app.use(express.static(__dirname + '/public'));
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+const players = {};
+
+app.use(express.static(`${__dirname}/public`));
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
 });
 
-io.on('connection', function (socket) {
-    console.log('user connected');
-    players[socket.id] = {
-        rotation: 0,
-        x: Math.floor(Math.random() * 700) + 50,
-        y: Math.floor(Math.random() * 500) + 50,
-        playerId: socket.id,
-        team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
-    };
-    socket.emit('currentPlayers', players);
-    socket.broadcast.emit('newPlayer', players[socket.id]);
-    socket.on('disconnect', function () {
+io.on('connection', (socket) => {
+  console.log('user connected');
+  players[socket.id] = {
+    rotation: 0,
+    x: Math.floor(Math.random() * 700) + 50,
+    y: Math.floor(Math.random() * 500) + 50,
+    playerId: socket.id,
+    team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue',
+  };
+  socket.emit('currentPlayers', players);
+  socket.broadcast.emit('newPlayer', players[socket.id]);
+  socket.on('disconnect', () => {
+    delete players[socket.id];
 
-        delete players[socket.id];
-
-        io.emit('disconnect', socket.id);
-        console.log('user disconnected');
-    });
+    io.emit('disconnect', socket.id);
+    console.log('user disconnected');
+  });
+  socket.on('playerMovement', (playerInfo) => {
+    players[socket.id].y = playerInfo.y;
+    players[socket.id].x = playerInfo.x;
+    players[socket.id].rotation = playerInfo.rotation;
+    socket.broadcast.emit('playerMoved', players[socket.id]);
+  });
 });
 
-server.listen(8081, function () {
-    console.log(`Listening on ${server.address().port}`);
+
+server.listen(8081, () => {
+  console.log(`Listening on ${server.address().port}`);
 });
-
-
